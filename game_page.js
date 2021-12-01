0f910392851867354bf7d5366857e6d9c8e1c4c1
Atharv4062 <88375467+Atharv4062@users.noreply.github.com>
2021-12-01 20:03:20 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -31,28 +31,27 @@ document.getElementById("player_question").innerHTML="Question Turn-"+player1_na
 document.getElementById("player_answer").innerHTML="Answer Turn-"+player2_name;
 
 question_turn= "player1";
-answer_turn= "player1";
+answer_turn= "player2";
 
 function check()
 {
     get_answer=document.getElementById("input_check_box").value;
     if(get_answer == actual_answer)
-    {
 
-    if(answer == word )
+
     {
         if(answer_turn == "player1")
          {
-        update_player1_score=player1_score+1;
+        player1_score=player1_score+1;
         document.getElementById("player1_score").innerHTML=player1_score;
      }
      else{
-         update_player2_score=player2_score+1;
+         player2_score=player2_score+1;
          document.getElementById("player2_score").innerHTML=player2_score;
      }
      
     }
-}
+
 
     if(question_turn == "player1")
     {
@@ -62,7 +61,7 @@ function check()
     }
     else{question_turn= "player1"
     document.getElementById("player_question").innerHTML="Question Turn-" + player1_name;
-     }
+}
 
 if(answer_turn == "player1")
     {
